a1c3091ece7d199a64a73cf898689f9df4a65ef4
ankit24695 <chaurasia.ankit773@gmail.com>
2022-09-27 23:08:39 +0530

    Changes in Customer

--- ShoppingApplication/src/main/java/com/shopping/module/Customer.java
+++ ShoppingApplication/src/main/java/com/shopping/module/Customer.java
@@ -16,45 +16,61 @@ public class Customer {
 	private String firstName;
 	private String lastName;
 	private String mobileNumber;
-	private String email;
+	
+	@OneToOne
+	private User user;
+	
 	@OneToOne(cascade = CascadeType.ALL)
 	private Address address;
+
 	public Integer getCustomerId() {
 		return customerId;
 	}
+
 	public void setCustomerId(Integer customerId) {
 		this.customerId = customerId;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getMobileNumber() {
 		return mobileNumber;
 	}
+
 	public void setMobileNumber(String mobileNumber) {
 		this.mobileNumber = mobileNumber;
 	}
-	public String getEmail() {
-		return email;
+
+	public User getUser() {
+		return user;
 	}
-	public void setEmail(String email) {
-		this.email = email;
+
+	public void setUser(User user) {
+		this.user = user;
 	}
+
 	public Address getAddress() {
 		return address;
 	}
+
 	public void setAddress(Address address) {
 		this.address = address;
 	}
+	
 	
 	
 	
--- ShoppingApplication/src/main/java/com/shopping/module/User.java
+++ ShoppingApplication/src/main/java/com/shopping/module/User.java
@@ -1,50 +1,40 @@
 package com.shopping.module;
 
-import javax.persistence.CascadeType;
-import javax.persistence.OneToOne;
-import javax.validation.constraints.NotNull;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+
+@Entity
 public class User {
-
+    @Id
+    @GeneratedValue(strategy =GenerationType.AUTO)
 	private Integer userId;
 	private String password;
 	private String role;
-	
-	@NotNull
-	@OneToOne(cascade = CascadeType.ALL)
-	private Customer customer;
-
 	public Integer getUserId() {
 		return userId;
 	}
-
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
-
 	public String getPassword() {
 		return password;
 	}
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
 	public String getRole() {
 		return role;
 	}
-
 	public void setRole(String role) {
 		this.role = role;
 	}
+	
+	
+	
 
-	public Customer getCustomer() {
-		return customer;
-	}
-
-	public void setCustomer(Customer customer) {
-		this.customer = customer;
-	}
 	
 	
 	
