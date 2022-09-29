3160cbc8c7252887f434d748f4579bb8ae4f772b
ankit24695 <chaurasia.ankit773@gmail.com>
2022-09-29 12:59:54 +0530

    Changes

--- ShoppingApplication/src/main/java/com/shopping/module/Cart.java
+++ ShoppingApplication/src/main/java/com/shopping/module/Cart.java
@@ -3,7 +3,7 @@ package com.shopping.module;
 import java.util.ArrayList;
 import java.util.List;
 
-
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -22,12 +22,18 @@ public class Cart {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer cartId;
 	
+	@Column(unique = true)
+	private Integer custId;
+	
 	@OneToOne
 	@JoinColumn(name = "customer_id", referencedColumnName = "customerId")
 	private Customer customer;
 	
 	@OneToMany(mappedBy = "cart")
 	private List<ProductDTO> list=new ArrayList<>();
+	
+	
+	private double totalAmount;
 	
 	public Integer getCartId() {
 		return cartId;
@@ -51,6 +57,22 @@ public class Cart {
 
 	public void setList(List<ProductDTO> list) {
 		this.list = list;
+	}
+
+	public Integer getCustomerId() {
+		return custId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		custId = custId;
+	}
+
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
 	}
 
 	
